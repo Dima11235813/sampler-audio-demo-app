53b99cac3a9c471ec329309b9cf88c7a748df969
Dmitri <LarionovDmitri@gmail.com>
2019-10-18 22:20:57 -0500

    moved functionality to react component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { main } from "./toneContext/toneContext";
+import AUD_CST from "./constants/audioConsts";
+import { AudioElement } from "./component/AudioElement";
 
 function App() {
-  // let updateSampleItems = useState()
-  // const handleStartRecording = () => {
-  //   // debugger;
-  //   main.start()
-  // };
-  // debugger;
+  let newMediaRec
+  const [blobUrls, addBlobUrl] = useState([]);
+  const gotUserMediaCb = stream => {
+    newMediaRec = new MediaRecorder(stream);
+    console.log(`Created new media recorder #${newMediaRec}`);
+    let tempChunks = []
+    newMediaRec.ondataavailable = e => {
+      // add stream data to chunks
+      tempChunks.push(e.data);
+      console.log(`Pushing chunks`);
+      console.log(e.data);
+      // if recorder is 'inactive' then recording has finished
+      if (newMediaRec.state == "inactive") {
+        // convert stream data chunks to a 'webm' audio format as a blob
+        const blob = new Blob(tempChunks, {
+          type: "audio/wav"
+        });
+        let blobUrl = URL.createObjectURL(blob);
+        addBlobUrl([...blobUrls, blobUrl]);
+      }
+    };
+    newMediaRec.start()
+  };
+  const createRecorder = () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(stream => {
+        gotUserMediaCb(stream);
+      })
+      .catch(err => {
+        console.log("fail to get user media");
+      });
+  };
+  const handleStartRecording = () => {
+    if(!newMediaRec){
+      createRecorder()
+    }else{
+      newMediaRec.start()
+    }
+  }
+  const handleStopRecording = () => {
+    newMediaRec.stop();
+  };
   return (
     <div className="App">
       <div id="sound-controls-containter">
-        <button onClick={main.start}>Start</button>
-        <button onClick={main.stop}>Stop</button>
+        <button onClick={handleStartRecording}>Start</button>
+        <button onClick={handleStopRecording}>Stop</button>
+        <div id="sound-controls-containter">
+          {blobUrls.map((blobUrl, index) => (
+            <AudioElement key={index} blobUrl={blobUrl} number={index + 1} />
+          ))}
+        </div>
       </div>
     </div>
   );
